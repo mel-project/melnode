0eca809076aab89f9e31297e96c73b22fccc1b51
So Qu <soqu@Sos-MacBook-Pro.local>
2020-12-07 11:35:52 -0800

    Adding struct opt dependency and opt struct to take in args

--- libs/symphonia/src/bin/symphtest.rs
+++ libs/symphonia/src/bin/symphtest.rs
@@ -1,7 +1,52 @@
-use std::time::Duration;
-
 use env_logger::Env;
+use std::path::PathBuf;
+use std::time::Duration;
+use structopt::StructOpt;
 use symphonia::testing::{Harness, MockNet};
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Symphonia Test Harness",
+    about = "Simulate a network of nodes running symphonia"
+)]
+struct Opt {
+    #[structopt(
+        name = "mean",
+        long,
+        short,
+        help = "Mean time in ms for latency",
+        default = "100"
+    )]
+    latency_mean_ms: u32,
+
+    #[structopt(
+        name = "variance",
+        long,
+        short,
+        help = "Variance time in ms for latency",
+        default = "10"
+    )]
+    latency_variance_ms: u32,
+
+    #[structopt(
+        name = "loss",
+        long,
+        short,
+        help = "Probability of loss per network transfer",
+        default = "0.01"
+    )]
+    loss_prob: f64,
+
+    #[structopt(
+        name = "weights",
+        long,
+        short,
+        help = "Comma separated voting weight of each consensus participants",
+        default_value = "100",
+        raw(use_delimiter = "true")
+    )]
+    participant_weights: Vec<u32>,
+}
 
 fn main() {
     smol::block_on(async move {
