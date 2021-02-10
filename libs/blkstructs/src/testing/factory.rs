10462c13eea3ef837aae9d7bbe62a25376673c6d
tavakyan <tigran.avakyan@icloud.com>
2021-02-10 02:27:27 -0800

    getting ride of old fixture/factory function

--- libs/blkstructs/src/testing/factory.rs
+++ libs/blkstructs/src/testing/factory.rs
@@ -92,31 +92,3 @@ beaver::define! {
         proposer_action -> |_| None,
     }
 }
-//
-// // TODO: get ride of this... doesnt' belong here should be a  fixture
-// pub fn tx_factory(
-//     kind: TxKind,
-//     sender_keypair: (Ed25519PK, Ed25519SK),
-//     dest_pk: Ed25519PK,
-//     coin_id: CoinID,
-//     script: melscript::Script,
-//     value: u128,
-//     fee: u128
-// ) -> Transaction {
-//     let tx = Transaction {
-//         kind,
-//         inputs: vec![coin_id],
-//         outputs: vec![CoinData {
-//             covhash: melscript::Script::std_ed25519_pk(dest_pk).hash(),
-//             value: value - fee,
-//             denom: DENOM_TMEL.to_owned(),
-//         }],
-//         fee,
-//         scripts: vec![script],
-//         data: vec![],
-//         sigs: vec![]
-//     };
-//
-//     // Sign transaction and return tx
-//     tx.sign_ed25519(sender_keypair.1)
-// }
