6d310f2dc9cb5d2de57c74a676171f78e6289475
tavakyan <tigran.avakyan@icloud.com>
2021-03-24 19:08:22 -0700

    Fixing broken things after master merge

--- commands/themelio-node/src/services/wallet/mod.rs
+++ commands/themelio-node/src/services/wallet/mod.rs
@@ -1,7 +1,7 @@
-mod active;
-mod available;
-mod data;
+// mod active;
+// mod available;
+// mod data;
 
-pub use active::ActiveWallet;
-pub use available::AvailableWallets;
-pub use data::WalletData;
+// pub use active::ActiveWallet;
+// pub use available::AvailableWallets;
+// pub use data::WalletData;
--- commands/themelio-node/src/tasks/node.rs
+++ commands/themelio-node/src/tasks/node.rs
@@ -42,7 +42,7 @@ pub async fn run_node(opt: NodeConfig) {
     log::info!("themelio-core v{} initializing...", VERSION);
     log::info!("bootstrapping with {:?}", opt.bootstrap);
     let storage =
-        NodeStorage::new(sled::open(&opt.database).unwrap(), testnet_genesis_config()).share();
+        NodeStorage::new(sled::open(&opt.database).unwrap(), testnet_genesis_config().await).share();
     let _node_prot = NodeProtocol::new(opt.listen, opt.bootstrap.clone(), storage.clone());
     let _staker_prot = if let Some(v) = opt.test_stakeholder {
         let my_sk = insecure_testnet_keygen(v).1;
