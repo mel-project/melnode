d215d91b9d60f814bff0aa233d40405e43926f51
nullchinchilla <nullchinchilla@pm.me>
2021-03-25 09:53:48 -0500

    nodeprot is now correct

--- commands/themelio-bxms/src/main.rs
+++ commands/themelio-bxms/src/main.rs
@@ -1,9 +1,10 @@
-use std::net::SocketAddr;
+use std::{convert::TryInto, net::SocketAddr};
 
 use blkstructs::NetID;
 use nodeprot::ValClient;
 use once_cell::sync::OnceCell;
 use structopt::StructOpt;
+use tmelcrypt::HashVal;
 
 mod raw;
 
@@ -27,6 +28,15 @@ pub static GLOB_CLIENT: OnceCell<ValClient> = OnceCell::new();
 async fn main_inner() -> anyhow::Result<()> {
     let args = Args::from_args();
     let client = ValClient::new(NetID::Testnet, args.connect);
+    // TODO read this from an argument
+    client.trust(
+        70000,
+        HashVal(
+            hex::decode("8e45c78d6cb6f70cc2ac91b9cefe6844ce98037b6254fa838cb94a650d923c70")?
+                .try_into()
+                .unwrap(),
+        ),
+    );
     GLOB_CLIENT
         .set(client)
         .expect("GLOB_CLIENT should only be initialized once");
