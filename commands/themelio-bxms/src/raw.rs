4c63bb29cedb2fa28f43359d8b469ef75deebec9
nullchinchilla <nullchinchilla@pm.me>
2021-04-07 15:57:05 -0500

    caching works

--- commands/themelio-bxms/src/raw.rs
+++ commands/themelio-bxms/src/raw.rs
@@ -1,23 +1,37 @@
 use std::convert::TryInto;
 
 use nodeprot::ValClient;
-use tide::Body;
+use std::fmt::Debug;
+use tide::{Body, StatusCode};
 use tmelcrypt::HashVal;
+
+fn to_badreq<E: Into<anyhow::Error> + Send + 'static + Sync + Debug>(e: E) -> tide::Error {
+    tide::Error::new(StatusCode::BadRequest, e)
+}
+
+fn to_badgateway<E: Into<anyhow::Error> + Send + 'static + Sync + Debug>(e: E) -> tide::Error {
+    log::warn!("bad upstream: {:#?}", e);
+    tide::Error::new(StatusCode::BadGateway, e)
+}
+
+fn notfound() -> tide::Error {
+    tide::Error::new(StatusCode::NotFound, anyhow::anyhow!("not found"))
+}
 
 /// Get the latest status
 #[tracing::instrument]
 pub async fn get_latest(req: tide::Request<ValClient>) -> tide::Result<Body> {
-    let last_snap = req.state().snapshot().await?;
+    let last_snap = req.state().snapshot().await.map_err(to_badgateway)?;
     Ok(Body::from_json(&last_snap.header())?)
 }
 
 /// Get a particular block header
 #[tracing::instrument]
 pub async fn get_header(req: tide::Request<ValClient>) -> tide::Result<Body> {
-    let height: u64 = req.param("height")?.parse()?;
-    let last_snap = req.state().snapshot().await?;
-    let older = last_snap.get_history(height).await?;
-    Ok(Body::from_json(&older)?)
+    let height: u64 = req.param("height")?.parse().map_err(to_badreq)?;
+    let last_snap = req.state().snapshot().await.map_err(to_badgateway)?;
+    let older = last_snap.get_history(height).await.map_err(to_badgateway)?;
+    Ok(Body::from_json(&older.ok_or_else(notfound)?)?)
 }
 
 /// Get a particular transaction
@@ -34,5 +48,5 @@ pub async fn get_transaction(req: tide::Request<ValClient>) -> tide::Result<Body
     let last_snap = req.state().snapshot().await?;
     let older = last_snap.get_older(height).await?;
     let tx = older.get_transaction(txhash).await?;
-    Ok(Body::from_json(&tx)?)
+    Ok(Body::from_json(&tx.ok_or_else(notfound)?)?)
 }
