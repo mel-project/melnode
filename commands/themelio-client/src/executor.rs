aecbb34742a274812ea9b26689444cd5a4b6b64e
tavakyan <tigran.avakyan@icloud.com>
2021-04-13 01:14:43 -0700

    ...

--- commands/themelio-client/src/executor.rs
+++ commands/themelio-client/src/executor.rs
@@ -33,35 +33,23 @@ impl CommandExecutor {
     /// The results of the faucet tx are shown to the user.
     pub async fn faucet(&self, wallet_name: &str, secret: &str, amount: &str, unit: &str) -> anyhow::Result<()> {
         let manager = WalletManager::new(&self.host.clone(), &self.database.clone());
-        let res = manager.faucet()
+        let res = manager.faucet().await?;
         SubShellOutput::show_faucet_tx().await?;
-        let wallet = manager.open(wallet_name, secret).await?;
-
-        wallet.faucet(amount)
-        manager.load_wallet(wallet_name).await?;
-
-        manager.faucet(wallet_name, secret, amount, unit)
-        let wallet = self.load_wallet()?;
-        let wallet.
-        wallet.faucet(&wallet, amount, unit).await?;
         Ok(())
     }
 
     /// Opens a wallet by name and secret and sends coins from the wallet to a destination.
     pub async fn send_coins(&self, wallet_name: &str, secret: &str, address: &str, amount: &str, unit: &str) -> anyhow::Result<()> {
-        wallet.send_coins(wallet_name).await?;
         Ok(())
     }
 
     /// Adds coins by coin id to wallet.
     pub async fn add_coins(&self, wallet_name: &str, secret: &str, coin_id: &str) -> anyhow::Result<()> {
-        wallet.add_coins(wallet_name).await?;
         Ok(())
     }
 
     /// Shows the total known wallet balance.
     pub async fn show_balance(&self, wallet_name: &str, secret: &str, ) -> anyhow::Result<()> {
-        wallet.show_balance().await?;
         Ok(())
     }
 
