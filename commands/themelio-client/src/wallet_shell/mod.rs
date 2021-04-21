0f5a1653fee47be3219654831ec75036ffcd376f
tavakyan <tigran.avakyan@icloud.com>
2021-04-21 00:45:23 -0700

    ...

--- commands/themelio-client/src/wallet_shell/mod.rs
+++ commands/themelio-client/src/wallet_shell/mod.rs
@@ -1,5 +1,4 @@
 pub mod command;
-pub mod executor;
 pub mod prompt;
 pub mod output;
 pub mod runner;
--- commands/themelio-client/src/wallet_shell/sub/mod.rs
+++ commands/themelio-client/src/wallet_shell/sub/mod.rs
@@ -2,4 +2,3 @@ pub mod command;
 pub mod prompt;
 pub mod output;
 pub mod runner;
-pub mod executor;
