09d1cf9c6b04fceabde3bde32f7d5324d289f7c2
Meade Kincke <thedarkula2049@gmail.com>
2021-07-19 10:44:50 -0600

    Added the biome plan and updated CI.

---
+++ biome/themelio-node/plan.sh
@@ -0,0 +1,46 @@
+pkg_name=themelio-node
+pkg_origin=themelio
+pkg_repository_name="themelio-core"
+pkg_maintainer="Meade Kincke <meade@themelio.org>"
+pkg_version="0.1.0"
+pkg_license=("MPL-2.0")
+pkg_full_path="${HAB_CACHE_SRC_PATH}/${pkg_name}-${pkg_version}/${pkg_repository_name}"
+pkg_build_deps=(
+  core/git
+  core/rust
+)
+pkg_deps=(
+  core/curl
+  core/gcc-libs
+)
+pkg_bin_dirs=(bin)
+pkg_exports=(
+  [port]=port
+)
+pkg_exposes=(port)
+
+do_verify() {
+  return 0
+}
+
+do_check() {
+  cargo test --locked --target x86_64-unknown-linux-musl --verbose
+}
+
+do_build() {
+  git clone "https://github.com/themeliolabs/${pkg_repository_name}.git" "/hab/cache/src/${pkg_name}-${pkg_version}/${pkg_repository_name}"
+
+  cd "${pkg_full_path}"
+
+  cargo build --locked --release --verbose
+}
+
+do_install() {
+  local release="${pkg_full_path}/target/release/${pkg_name}"
+  local target="${pkg_prefix}/target"
+  local application_path="${pkg_prefix}/bin/"
+
+  mv  "$release" "$application_path"
+
+  rm -rf "$target"
+}
---
+++ biome/themelio-node/tests/test.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+set -e
+
+TESTDIR="$(dirname "${0}")"
+PLANDIR="$(dirname "${TESTDIR}")"
+
+bio pkg install --binlink core/bats
+bio pkg install --binlink core/nmap
+
+source "${PLANDIR}/plan.sh"
+
+if [ -n "${SKIPBUILD}" ]; then
+  source results/last_build.env
+
+  BIO_SVC_STATUS="$(bio svc status)"
+  NO_SERVICES_LOADED="No services loaded."
+
+  if [ "$BIO_SVC_STATUS" == "$NO_SERVICES_LOADED" ]; then
+    bio pkg install --binlink --force "results/${pkg_artifact}"
+    bio svc load "${pkg_ident}"
+  else
+    bio svc unload "${pkg_ident}" || true
+    bio pkg install --binlink --force "results/${pkg_artifact}"
+    sleep 1
+    bio svc load "${pkg_ident}"
+  fi
+else
+  pushd "${PLANDIR}"
+  build
+  popd
+
+  source results/last_build.env
+
+  BIO_SVC_STATUS="$(bio svc status)"
+  NO_SERVICES_LOADED="No services loaded."
+
+  if [ "$BIO_SVC_STATUS" == "$NO_SERVICES_LOADED" ]; then
+    bio pkg install --binlink --force "results/${pkg_artifact}"
+    bio svc load "${pkg_ident}"
+  else
+    bio svc unload "${pkg_ident}" || true
+    bio pkg install --binlink --force "results/${pkg_artifact}"
+    sleep 1
+    bio svc load "${pkg_ident}"
+  fi
+fi
+
+echo "Sleeping for 5 seconds for the service to start."
+sleep 5
+
+bats "${TESTDIR}/test.bats"
+
+bio svc unload "${pkg_ident}" || true
