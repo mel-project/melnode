fca91106ff519637608b6cd74d4b86827a1b8af8
Meade Kincke <thedarkula2049@gmail.com>
2021-07-20 14:13:50 -0600

    Working on CI.

--- biome/themelio-node/tests/test-interactive.sh
+++ biome/themelio-node/tests/test-interactive.sh
@@ -5,8 +5,8 @@ set -ex
 TEST_DIR="$(dirname "${0}")"
 PLAN_DIR="$(dirname "${TEST_DIR}")"
 
-sudo bio pkg install --binlink core/bats
-sudo bio pkg install --binlink core/nmap
+bio pkg install --binlink core/bats
+bio pkg install --binlink core/nmap
 
 source "${PLAN_DIR}/plan.sh"
 
@@ -17,16 +17,16 @@ if [ -n "${SKIP_BUILD}" ]; then
   NO_SERVICES_LOADED="No services loaded."
 
   if [ "$BIO_SVC_STATUS" == "$NO_SERVICES_LOADED" ]; then
-    sudo bio pkg install --binlink --force "/src/results/${pkg_artifact}"
+    bio pkg install --binlink --force "results/${pkg_artifact}"
     bio svc load "${pkg_ident}"
   else
     bio svc unload "${pkg_ident}" || true
-    bio pkg install --binlink --force "/src/results/${pkg_artifact}"
+    bio pkg install --binlink --force "results/${pkg_artifact}"
     sleep 1
     bio svc load "${pkg_ident}"
   fi
 else
-  bio pkg build "${pkg_name}"
+  bio pkg build "biome/${pkg_name}"
 
   source /src/results/last_build.env
 
@@ -34,11 +34,11 @@ else
   NO_SERVICES_LOADED="No services loaded."
 
   if [ "$BIO_SVC_STATUS" == "$NO_SERVICES_LOADED" ]; then
-    sudo bio pkg install --binlink --force "/src/results/${pkg_artifact}"
+    bio pkg install --binlink --force "results/${pkg_artifact}"
     bio svc load "${pkg_ident}"
   else
     bio svc unload "${pkg_ident}" || true
-    sudo bio pkg install --binlink --force "/src/results/${pkg_artifact}"
+    bio pkg install --binlink --force "results/${pkg_artifact}"
     sleep 1
     bio svc load "${pkg_ident}"
   fi
