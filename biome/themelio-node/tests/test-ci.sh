eb3e60faad2d66b3651403214399d6c2a9dc51cd
Meade Kincke <thedarkula2049@gmail.com>
2021-07-20 14:23:49 -0600

    Working on CI.

---
+++ biome/themelio-node/tests/test-ci.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -ex
+
+TEST_DIR="$(dirname "${0}")"
+PLAN_DIR="$(dirname "${TEST_DIR}")"
+
+sudo bio pkg install --binlink core/bats
+sudo bio pkg install --binlink core/nmap
+
+source "${PLAN_DIR}/plan.sh"
+
+bio pkg build "${pkg_name}"
+
+ls -la /src
+
+ls -la /src/results
+
+source /src/results/last_build.env
+
+BIO_SVC_STATUS="$(bio svc status)"
+NO_SERVICES_LOADED="No services loaded."
+
+if [ "$BIO_SVC_STATUS" == "$NO_SERVICES_LOADED" ]; then
+  sudo bio pkg install --binlink --force "/src/results/${pkg_artifact}"
+  bio svc load "${pkg_ident}"
+else
+  bio svc unload "${pkg_ident}" || true
+  sudo bio pkg install --binlink --force "/src/results/${pkg_artifact}"
+  sleep 1
+  bio svc load "${pkg_ident}"
+fi
+
+echo "Sleeping for 5 seconds for the service to start."
+sleep 5
+
+bats "${TEST_DIR}/test.bats"
+
+bio svc unload "${pkg_ident}" || true
