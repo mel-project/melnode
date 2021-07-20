ca9d04f9233599fc81eb582ec4faa326d570aa4e
Meade Kincke <thedarkula2049@gmail.com>
2021-07-20 15:01:55 -0600

    Working on CI.

--- biome/themelio-node/tests/test-ci.sh
+++ biome/themelio-node/tests/test-ci.sh
@@ -14,24 +14,24 @@ bio pkg build "${pkg_name}"
 
 source results/last_build.env
 
-bio sup run
+sudo bio sup run
 
-BIO_SVC_STATUS="$(bio svc status)"
-NO_SERVICES_LOADED="No services loaded."
-
-if [ "$BIO_SVC_STATUS" == "$NO_SERVICES_LOADED" ]; then
-  sudo bio pkg install --binlink --force "results/${pkg_artifact}"
-  bio svc load "${pkg_ident}"
-else
-  bio svc unload "${pkg_ident}" || true
-  sudo bio pkg install --binlink --force "results/${pkg_artifact}"
-  sleep 1
-  bio svc load "${pkg_ident}"
-fi
-
-echo "Sleeping for 5 seconds for the service to start."
-sleep 5
-
-bats "${TEST_DIR}/test.bats"
-
-bio svc unload "${pkg_ident}" || true
+#BIO_SVC_STATUS="$(bio svc status)"
+#NO_SERVICES_LOADED="No services loaded."
+#
+#if [ "$BIO_SVC_STATUS" == "$NO_SERVICES_LOADED" ]; then
+#  sudo bio pkg install --binlink --force "results/${pkg_artifact}"
+#  bio svc load "${pkg_ident}"
+#else
+#  bio svc unload "${pkg_ident}" || true
+#  sudo bio pkg install --binlink --force "results/${pkg_artifact}"
+#  sleep 1
+#  bio svc load "${pkg_ident}"
+#fi
+#
+#echo "Sleeping for 5 seconds for the service to start."
+#sleep 5
+#
+#bats "${TEST_DIR}/test.bats"
+#
+#bio svc unload "${pkg_ident}" || true
