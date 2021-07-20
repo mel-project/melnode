9b8974288762ce538a2ff023af30966e4ddc405d
Meade Kincke <thedarkula2049@gmail.com>
2021-07-20 15:45:22 -0600

    Working on CI.

--- biome/themelio-node/tests/test-ci.sh
+++ biome/themelio-node/tests/test-ci.sh
@@ -10,13 +10,9 @@ sudo bio pkg install --binlink core/nmap
 
 source "${PLAN_DIR}/plan.sh"
 
-#bio pkg build "${pkg_name}"
+bio pkg build "${pkg_name}"
 
-#source results/last_build.env
-
-#sudo bio pkg install --binlink biome/bio-sup
-#sudo bio pkg install --binlink biome/bio-launcher
-#bio sup run
+source results/last_build.env
 
 sudo bio sup run &
 
@@ -25,22 +21,22 @@ sleep 5
 sudo bio svc status
 
 
-#BIO_SVC_STATUS="$(bio svc status)"
-#NO_SERVICES_LOADED="No services loaded."
-#
-#if [ "$BIO_SVC_STATUS" == "$NO_SERVICES_LOADED" ]; then
-#  sudo bio pkg install --binlink --force "results/${pkg_artifact}"
-#  bio svc load "${pkg_ident}"
-#else
-#  bio svc unload "${pkg_ident}" || true
-#  sudo bio pkg install --binlink --force "results/${pkg_artifact}"
-#  sleep 1
-#  bio svc load "${pkg_ident}"
-#fi
-#
-#echo "Sleeping for 5 seconds for the service to start."
-#sleep 5
-#
-#bats "${TEST_DIR}/test.bats"
-#
-#bio svc unload "${pkg_ident}" || true
+BIO_SVC_STATUS="$(bio svc status)"
+NO_SERVICES_LOADED="No services loaded."
+
+if [ "$BIO_SVC_STATUS" == "$NO_SERVICES_LOADED" ]; then
+  sudo bio pkg install --binlink --force "results/${pkg_artifact}"
+  bio svc load "${pkg_ident}"
+else
+  bio svc unload "${pkg_ident}" || true
+  sudo bio pkg install --binlink --force "results/${pkg_artifact}"
+  sleep 1
+  bio svc load "${pkg_ident}"
+fi
+
+echo "Sleeping for 5 seconds for the service to start."
+sleep 5
+
+bats "${TEST_DIR}/test.bats"
+
+bio svc unload "${pkg_ident}" || true
