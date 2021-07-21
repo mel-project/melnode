02c4abdda510568021a933e45e38bb2e877c98bc
Meade Kincke <thedarkula2049@gmail.com>
2021-07-21 14:56:03 -0600

    Working on CI.

---
+++ biome/themelio-node/scripts/ci-test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+TEST_DIR="$(dirname "${0}")"
+PLAN_DIR="$(dirname "${TEST_DIR}")"
+
+sudo bio pkg install --binlink core/bats
+sudo bio pkg install --binlink core/nmap
+
+source "${PLAN_DIR}/plan.sh"
+
+sudo bio sup run &
+
+bio pkg build "${pkg_name}"
+
+source results/last_build.env
+
+sudo bio pkg install --binlink --force "results/${pkg_artifact}"
+
+sudo useradd hab -s /bin/bash -p '*'
+
+sudo bio svc load "${pkg_ident}"
+
+echo "Sleeping for 5 seconds for the service to start."
+sleep 5
+
+bats "${TEST_DIR}/test.bats"
+
+sudo bio svc unload "${pkg_ident}" || true
