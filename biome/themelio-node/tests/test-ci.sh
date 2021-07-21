f35414c150340a794277a96c61b30e32d01b9ce6
Meade Kincke <thedarkula2049@gmail.com>
2021-07-21 12:26:25 -0600

    Working on CI.

--- biome/themelio-node/tests/test-ci.sh
+++ biome/themelio-node/tests/test-ci.sh
@@ -12,9 +12,6 @@ source "${PLAN_DIR}/plan.sh"
 
 sudo bio sup run &
 
-#bio origin key download themelio
-#sudo bio origin key download themelio
-
 bio pkg build "${pkg_name}"
 
 source results/last_build.env
@@ -32,4 +29,4 @@ bats "${TEST_DIR}/test.bats"
 
 sudo bio svc unload "${pkg_ident}" || true
 
-bio pkg export container "results/${pkg_artifact}"
+sudo bio pkg export container "results/${pkg_artifact}"
