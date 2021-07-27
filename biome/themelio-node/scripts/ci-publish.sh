1f58bdbab561933d39f9e6604538e27e7beb10be
Meade Kincke <thedarkula2049@gmail.com>
2021-07-27 10:04:29 -0600

    Complete packer implementation, separation of master and pull request scripts.

--- biome/themelio-node/scripts/ci-publish.sh
+++ biome/themelio-node/scripts/ci-publish.sh
@@ -2,34 +2,16 @@
 
 set -ex
 
-TEST_DIR="$(dirname "${0}")"
 PLAN_DIR="$(dirname "${TEST_DIR}")"
-
-sudo bio pkg install --binlink core/bats
-sudo bio pkg install --binlink core/nmap
 
 source "${PLAN_DIR}/plan.sh"
 
-sudo bio sup run &
 
 bio pkg build "${pkg_name}"
 
 source results/last_build.env
 
 hart_file="results/${pkg_artifact}"
-
-sudo bio pkg install --binlink --force "${hart_file}"
-
-sudo useradd hab -s /bin/bash -p '*'
-
-sudo bio svc load "${pkg_ident}"
-
-echo "Sleeping for 5 seconds for the service to start."
-sleep 5
-
-bats "${TEST_DIR}/test.bats"
-
-sudo bio svc unload "${pkg_ident}" || true
 
 
 echo "Publishing artifact to the stable channel"
@@ -49,3 +31,43 @@ for tag in ${tags//,/ }; do
 
 	docker push "${local_tag}"
 done
+
+
+echo "Creating packer images"
+
+# We have these run with `&` so that they run in parallel. The openstack CLI is horrifically slow.
+
+# Flavour per region
+export BHS5_FLAVOUR="$(env OS_REGION_NAME=BHS5 openstack flavor list -f json | jq -r '.[] | select(.Name == "d2-2") | .ID')" &
+export DE1_FLAVOUR="$(env OS_REGION_NAME=DE1 openstack flavor list -f json | jq -r '.[] | select(.Name == "d2-2") | .ID')" &
+export GRA5_FLAVOUR="$(env OS_REGION_NAME=GRA5 openstack flavor list -f json | jq -r '.[] | select(.Name == "d2-2") | .ID')" &
+export GRA11_FLAVOUR="$(env OS_REGION_NAME=GRA11 openstack flavor list -f json | jq -r '.[] | select(.Name == "d2-2") | .ID')" &
+export SBG5_FLAVOUR="$(env OS_REGION_NAME=SBG5 openstack flavor list -f json | jq -r '.[] | select(.Name == "d2-2") | .ID')" &
+export UK1_FLAVOUR="$(env OS_REGION_NAME=UK1 openstack flavor list -f json | jq -r '.[] | select(.Name == "d2-2") | .ID')" &
+export WAW1_FLAVOUR="$(env OS_REGION_NAME=WAW1 openstack flavor list -f json | jq -r '.[] | select(.Name == "d2-2") | .ID')" &
+
+# Image ID per region
+export BHS5_IMAGE_ID="$(env OS_REGION_NAME=BHS5 openstack image list -f json | jq -r '.[] | select(.Name == "Archlinux") | .ID')" &
+export DE1_IMAGE_ID="$(env OS_REGION_NAME=DE1 openstack image list -f json | jq -r '.[] | select(.Name == "Archlinux") | .ID')" &
+export GRA5_IMAGE_ID="$(env OS_REGION_NAME=GRA5 openstack image list -f json | jq -r '.[] | select(.Name == "Archlinux") | .ID')" &
+export GRA11_IMAGE_ID="$(env OS_REGION_NAME=GRA11 openstack image list -f json | jq -r '.[] | select(.Name == "Archlinux") | .ID')" &
+export SBG5_IMAGE_ID="$(env OS_REGION_NAME=SBG5 openstack image list -f json | jq -r '.[] | select(.Name == "Archlinux") | .ID')" &
+export UK1_IMAGE_ID="$(env OS_REGION_NAME=UK1 openstack image list -f json | jq -r '.[] | select(.Name == "Archlinux") | .ID')" &
+export WAW1_IMAGE_ID="$(env OS_REGION_NAME=WAW1 openstack image list -f json | jq -r '.[] | select(.Name == "Archlinux") | .ID')" &
+
+# Network ID per region
+export BHS5_NETWORK_ID="$(env OS_REGION_NAME=BHS5 openstack network list -f json | jq -r '.[] | select(.Name == "Ext-Net") | .ID')" &
+export DE1_NETWORK_ID="$(env OS_REGION_NAME=DE1 openstack network list -f json | jq -r '.[] | select(.Name == "Ext-Net") | .ID')" &
+export GRA5_NETWORK_ID="$(env OS_REGION_NAME=GRA5 openstack network list -f json | jq -r '.[] | select(.Name == "Ext-Net") | .ID')" &
+export GRA11_NETWORK_ID="$(env OS_REGION_NAME=GRA11 openstack network list -f json | jq -r '.[] | select(.Name == "Ext-Net") | .ID')" &
+export SBG5_NETWORK_ID="$(env OS_REGION_NAME=SBG5 openstack network list -f json | jq -r '.[] | select(.Name == "Ext-Net") | .ID')" &
+export UK1_NETWORK_ID="$(env OS_REGION_NAME=UK1 openstack network list -f json | jq -r '.[] | select(.Name == "Ext-Net") | .ID')" &
+export WAW1_NETWORK_ID="$(env OS_REGION_NAME=WAW1 openstack network list -f json | jq -r '.[] | select(.Name == "Ext-Net") | .ID')" &
+
+wait
+
+envsubst < themelio-node.pkr.hcl.temp > themelio-node.pkr.hcl
+
+packer validate themelio-node.pkr.hcl
+
+packer build themelio-node.pkr.hcl
