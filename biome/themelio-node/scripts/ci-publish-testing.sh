cbeaaf3d9e77f44b0b7e25a2f128f92b494db9a5
Meade Kincke <thedarkula2049@gmail.com>
2021-09-15 11:22:03 -0600

    Debugging CI.

---
+++ biome/themelio-node/scripts/ci-publish-testing.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -ex
+
+export SCRIPTS_DIRECTORY="$(dirname "${0}")"
+PLAN_DIRECTORY="$(dirname "${SCRIPTS_DIRECTORY}")"
+
+cp "${PLAN_DIRECTORY}/plan-release.sh" "${PLAN_DIRECTORY}/plan.sh"
+
+source "${PLAN_DIRECTORY}/plan.sh"
+
+
+bio pkg build "biome/${pkg_name}"
+
+source results/last_build.env
+
+hart_file="results/${pkg_artifact}"
+
+
+echo "Publishing artifact to the stable channel"
+
+bio pkg upload --auth "${HABITAT_AUTH_TOKEN}" --url "${HAB_BLDR_URL}" "${hart_file}"
+bio pkg upload --auth "${BIOME_AUTH_TOKEN}" --url "${BIOME_BLDR_URL}" "${hart_file}"
+
+
+echo "Exporting docker image"
+sudo bio pkg export container "${hart_file}"
+
+source results/last_container_export.env
+
+for tag in ${tags//,/ }; do
+  local_tag="ghcr.io/themeliolabs/themelio-node:${tag}"
+
+  docker tag "${name}:${tag}" "${local_tag}"
+
+	docker push "${local_tag}"
+done
