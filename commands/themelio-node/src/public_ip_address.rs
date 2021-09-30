08d413bc3ea1ddf6e482c5f275b561b8eb419282
Meade Kincke <thedarkula2049@gmail.com>
2021-09-30 14:25:02 -0600

    Added a fix for an async failure.

--- commands/themelio-node/src/public_ip_address.rs
+++ commands/themelio-node/src/public_ip_address.rs
@@ -1,5 +1,12 @@
+use async_compat::CompatExt;
 use once_cell::sync::Lazy;
 
+// like smol::future::block_on(some_future.compat()). Of course that's a trait that you must use: use async_compat::CompatExt;
+// [2:18 PM]
+
+
 pub static PUBLIC_IP_ADDRESS: Lazy<String> = Lazy::new(|| {
-    smol::future::block_on(public_ip::addr()).expect("Could not obtain the public IP address.").to_string()
+    // Compat::new(crate::prometheus::prometheus()).await;
+    smol::future::block_on(public_ip::addr().compat()).expect("Could not obtain the public IP address.").to_string()
+    // smol::future::block_on(some_future.compat())
 });
