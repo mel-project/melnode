fbb94a031776ff8f348575250fa66477a1ed8e63
Meade Kincke <thedarkula2049@gmail.com>
2022-01-18 12:23:37 -0800

    Updated the manual packer build script.

--- biome/themelio-node/scripts/manual_packer_build.sh
+++ biome/themelio-node/scripts/manual_packer_build.sh
@@ -4,7 +4,7 @@ set -ex
 
 export SCRIPTS_DIRECTORY="$(dirname "${0}")"
 
-echo "Creating packer images"
+echo "Joining packer templates"
 
 sed -e '$s/$/\n/' -s ${SCRIPTS_DIRECTORY}/packer/*.hcl > ${SCRIPTS_DIRECTORY}/themelio-node-debian-aws.pkr.hcl.temp
 
@@ -12,6 +12,10 @@ envsubst < "${SCRIPTS_DIRECTORY}/themelio-node-debian-aws.pkr.hcl.temp" > "${SCR
 
 rm themelio-node-debian-aws.pkr.hcl.temp
 
+echo "Validating packer template"
+
 packer validate "${SCRIPTS_DIRECTORY}/themelio-node-debian-aws.pkr.hcl"
+
+echo "Building packer images"
 
 packer build "${SCRIPTS_DIRECTORY}/themelio-node-debian-aws.pkr.hcl"
