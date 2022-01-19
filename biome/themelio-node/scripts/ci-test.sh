a64a8d0d888895ac7d143b789bccc390d707fa23
Meade Kincke <thedarkula2049@gmail.com>
2022-01-19 09:28:17 -0800

    Updated the CI scripts to match the manual packer build.

--- biome/themelio-node/scripts/ci-test.sh
+++ biome/themelio-node/scripts/ci-test.sh
@@ -36,3 +36,27 @@ else
   sudo bio svc unload "${pkg_ident}"
   exit 1
 fi
+
+mkdir -p ${SCRIPTS_DIRECTORY}/packer/temporary-templates
+
+for region in $(cat $SCRIPTS_DIRECTORY/packer/aws_regions); do
+  echo "Creating packer templates for the $region region."
+
+  export AWS_REGION=${region}
+
+  envsubst < "${SCRIPTS_DIRECTORY}/packer/mainnet.pkr.hcl.temp" > "${SCRIPTS_DIRECTORY}/packer/temporary-templates/mainnet-$region.pkr.hcl"
+  envsubst < "${SCRIPTS_DIRECTORY}/packer/testnet.pkr.hcl.temp" > "${SCRIPTS_DIRECTORY}/packer/temporary-templates/testnet-$region.pkr.hcl"
+done
+
+cp ${SCRIPTS_DIRECTORY}/packer/00-base-image.pkr.hcl ${SCRIPTS_DIRECTORY}/packer/temporary-templates/
+
+echo "Joining packer templates"
+sed -e '$s/$/\n/' -s ${SCRIPTS_DIRECTORY}/packer/temporary-templates/*.hcl > ${SCRIPTS_DIRECTORY}/themelio-node-debian-aws.pkr.hcl.temp
+envsubst < "${SCRIPTS_DIRECTORY}/themelio-node-debian-aws.pkr.hcl.temp" > "${SCRIPTS_DIRECTORY}/themelio-node-debian-aws.pkr.hcl"
+
+echo "Cleaning up temporary files"
+rm themelio-node-debian-aws.pkr.hcl.temp
+rm -rf ${SCRIPTS_DIRECTORY}/packer/temporary-templates
+
+echo "Validating packer template"
+packer validate "${SCRIPTS_DIRECTORY}/themelio-node-debian-aws.pkr.hcl"
