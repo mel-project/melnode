3a777ff4af9ce8790e15b57a9b25943dda63ae67
Meade Kincke <thedarkula2049@gmail.com>
2022-01-27 12:11:16 -0800

    Uncommented a build block.

--- biome/themelio-node/scripts/ci-test.sh
+++ biome/themelio-node/scripts/ci-test.sh
@@ -23,37 +23,37 @@ fi
 export SCRIPTS_DIRECTORY="$(dirname "${0}")"
 PLAN_DIRECTORY="$(dirname "${SCRIPTS_DIRECTORY}")"
 
-#sudo bio pkg install --binlink themelio/bats
-#sudo bio pkg install --binlink core/curl
-#sudo bio pkg install --binlink core/nmap
-#
-#cp "${PLAN_DIRECTORY}/plan-debug.sh" "${PLAN_DIRECTORY}/plan.sh"
-#
-#source "${PLAN_DIRECTORY}/plan.sh"
-#
-#sudo bio sup run &
-#
-#bio pkg build "biome/${pkg_name}"
-#
-#source results/last_build.env
-#
-#sudo bio pkg install --binlink --force "results/${pkg_artifact}"
-#
-#sudo useradd hab -s /bin/bash -p '*'
-#
-#export DISABLE_HEALTH_CHECK=true
-#
-#sudo bio svc load "${pkg_ident}"
-#
-#echo "Sleeping for 5 seconds for the service to start."
-#sleep 5
-#
-#if bats --print-output-on-failure "${SCRIPTS_DIRECTORY}/test.bats"; then
-#  sudo bio svc unload "${pkg_ident}"
-#else
-#  sudo bio svc unload "${pkg_ident}"
-#  exit 1
-#fi
+sudo bio pkg install --binlink themelio/bats
+sudo bio pkg install --binlink core/curl
+sudo bio pkg install --binlink core/nmap
+
+cp "${PLAN_DIRECTORY}/plan-debug.sh" "${PLAN_DIRECTORY}/plan.sh"
+
+source "${PLAN_DIRECTORY}/plan.sh"
+
+sudo bio sup run &
+
+bio pkg build "biome/${pkg_name}"
+
+source results/last_build.env
+
+sudo bio pkg install --binlink --force "results/${pkg_artifact}"
+
+sudo useradd hab -s /bin/bash -p '*'
+
+export DISABLE_HEALTH_CHECK=true
+
+sudo bio svc load "${pkg_ident}"
+
+echo "Sleeping for 5 seconds for the service to start."
+sleep 5
+
+if bats --print-output-on-failure "${SCRIPTS_DIRECTORY}/test.bats"; then
+  sudo bio svc unload "${pkg_ident}"
+else
+  sudo bio svc unload "${pkg_ident}"
+  exit 1
+fi
 
 mkdir -p ${SCRIPTS_DIRECTORY}/packer/temporary-templates
 
@@ -70,9 +70,6 @@ if [ "${NETWORK_TO_BUILD}" == "mainnet" ]; then
   echo "Joining packer templates"
   sed -e '$s/$/\n/' -s ${SCRIPTS_DIRECTORY}/packer/temporary-templates/*.hcl > ${SCRIPTS_DIRECTORY}/themelio-node-mainnet-debian-aws.pkr.hcl.temp
   envsubst < "${SCRIPTS_DIRECTORY}/themelio-node-mainnet-debian-aws.pkr.hcl.temp" > "${SCRIPTS_DIRECTORY}/themelio-node-mainnet-debian-aws.pkr.hcl"
-
-  echo "Listing temporary-templates"
-  ls -la ${SCRIPTS_DIRECTORY}/packer/temporary-templates/
 
   echo "Cleaning up temporary files"
   rm ${SCRIPTS_DIRECTORY}/themelio-node-mainnet-debian-aws.pkr.hcl.temp
@@ -94,9 +91,6 @@ elif [ "${NETWORK_TO_BUILD}" == "testnet" ]; then
     echo "Joining packer templates"
     sed -e '$s/$/\n/' -s ${SCRIPTS_DIRECTORY}/packer/temporary-templates/*.hcl > ${SCRIPTS_DIRECTORY}/themelio-node-testnet-debian-aws.pkr.hcl.temp
     envsubst < "${SCRIPTS_DIRECTORY}/themelio-node-testnet-debian-aws.pkr.hcl.temp" > "${SCRIPTS_DIRECTORY}/themelio-node-testnet-debian-aws.pkr.hcl"
-
-    echo "Listing temporary-templates"
-    ls -la ${SCRIPTS_DIRECTORY}/packer/temporary-templates/
 
     echo "Cleaning up temporary files"
     rm ${SCRIPTS_DIRECTORY}/themelio-node-testnet-debian-aws.pkr.hcl.temp
