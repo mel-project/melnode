3e9dc46772cf4dd3de360d7353c33e7ae399f9c9
Meade <thedarkula2049@gmail.com>
2022-01-27 16:51:18 -0800

    Marry Up The Testnet Branch To Master (#76)
    * Split CI To Build Testnet And Mainnet Images Based On Branches (#73)
    
    * Added a testnet branch flag to part of the CI.
    
    * Testing syntax.
    
    * Split the CI into networks.
    
    * Working on the CI scripts.
    
    * Working on the CI scripts.
    
    * Uncommented a build block.
    
    * Added network comments.
    
    * Updated the CI publish script.
    
    * Made a CI file consistent with the rest.
    
    * Finishing CI Network Split (#75)
    
    * Updated the how-to-use instructions and split compose files into networks.
    
    * Split the biome plans by network.
    
    * Updated the docker-compose files.
    
    * Bunches of updates to split everything by network.
    
    * Updated the run hooks.
    
    * Added a missing flag.
    
    * Removed an old file.
    
    * Updated the CI test script.
    
    * Updated the CI test script.
    
    * Working on the CI test script.
    
    * Trying a fix.
    
    * Trying a fix.
    
    * Reverted a change.
    
    * Working on the CI test script.
    
    * Working on the CI test script.
    
    * Updated a gitignore.
    
    * More updates.
    
    * Updated the local test script.
    
    * Updated the CI test script.
    
    * Updated the CI publish script.
    
    * Trying a fix for the publish script.

--- biome/themelio-node/scripts/ci-publish.sh
+++ biome/themelio-node/scripts/ci-publish.sh
@@ -6,6 +6,9 @@ export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION
 
+export SCRIPTS_DIRECTORY="$(dirname "${0}")"
+PLAN_DIRECTORY="$(dirname "${SCRIPTS_DIRECTORY}")"
+
 if [ -z "${PROMTAIL_USERNAME}" ]; then
   echo "The PROMTAIL_USERNAME environment variable must be set."
   echo "Exiting."
@@ -20,47 +23,55 @@ if [ -z "${PROMTAIL_PASSWORD}" ]; then
   exit 1
 fi
 
-export SCRIPTS_DIRECTORY="$(dirname "${0}")"
-PLAN_DIRECTORY="$(dirname "${SCRIPTS_DIRECTORY}")"
+if [ "${NETWORK_TO_BUILD}" == "mainnet" ]; then
+  cp "${PLAN_DIRECTORY}/plan-debug-mainnet.sh" "${PLAN_DIRECTORY}/plan.sh"
+  cp -r "${PLAN_DIRECTORY}/hooks-mainnet" "${PLAN_DIRECTORY}/hooks"
 
-cp "${PLAN_DIRECTORY}/plan-release.sh" "${PLAN_DIRECTORY}/plan.sh"
+elif [ "${NETWORK_TO_BUILD}" == "testnet" ]; then
+  cp "${PLAN_DIRECTORY}/plan-debug-testnet.sh" "${PLAN_DIRECTORY}/plan.sh"
+  cp -r "${PLAN_DIRECTORY}/hooks-testnet" "${PLAN_DIRECTORY}/hooks"
+
+else
+  echo "No network specified with NETWORK_TO_BUILD. Exiting."
+  exit 1
+fi
+
 
 source "${PLAN_DIRECTORY}/plan.sh"
 
-
-bio pkg build "biome/${pkg_name}"
+bio pkg build "${PLAN_DIRECTORY}"
 
 source results/last_build.env
 
 hart_file="results/${pkg_artifact}"
 
-
-echo "Publishing artifact to the stable channel"
-
-bio pkg upload --auth "${HABITAT_AUTH_TOKEN}" --url "${HAB_BLDR_URL}" "${hart_file}" -c stable
-
-# Uploading to the biome builder is currently broken.
-#This can be uncommented when this bug report is remedied: https://github.com/biome-sh/biome/issues/14
-#bio pkg upload --auth "${BIOME_AUTH_TOKEN}" --url "${BIOME_BLDR_URL}" "${hart_file}" -c stable
-
-
-echo "Exporting docker image"
-sudo bio pkg export container "${hart_file}"
-
-source results/last_container_export.env
-
-for tag in ${tags//,/ }; do
-  local_tag="ghcr.io/themeliolabs/themelio-node:${tag}"
-
-  docker tag "${name}:${tag}" "${local_tag}"
-
-	docker push "${local_tag}"
-done
-
-
 mkdir -p ${SCRIPTS_DIRECTORY}/packer/temporary-templates
 
+
 if [ "${NETWORK_TO_BUILD}" == "mainnet" ]; then
+  echo "Publishing mainnet artifact to the stable channel"
+
+  bio pkg upload --auth "${HABITAT_AUTH_TOKEN}" --url "${HAB_BLDR_URL}" "${hart_file}" -c stable
+
+  # Uploading to the biome builder is currently broken.
+  #This can be uncommented when this bug report is remedied: https://github.com/biome-sh/biome/issues/14
+  #bio pkg upload --auth "${BIOME_AUTH_TOKEN}" --url "${BIOME_BLDR_URL}" "${hart_file}" -c stable
+
+
+  echo "Exporting mainnet docker image"
+  sudo bio pkg export container "${hart_file}"
+
+  source results/last_container_export.env
+
+  for tag in ${tags//,/ }; do
+    local_tag="ghcr.io/themeliolabs/themelio-node-mainnet:${tag}"
+
+    docker tag "${name}:${tag}" "${local_tag}"
+
+  	docker push "${local_tag}"
+  done
+
+
   for region in $(cat $SCRIPTS_DIRECTORY/packer/aws_regions); do
     echo "Creating packer templates for $region-$NETWORK_TO_BUILD."
 
@@ -85,6 +96,28 @@ if [ "${NETWORK_TO_BUILD}" == "mainnet" ]; then
   packer build "${SCRIPTS_DIRECTORY}/themelio-node-mainnet-debian-aws.pkr.hcl"
 
 elif [ "${NETWORK_TO_BUILD}" == "testnet" ]; then
+  echo "Publishing testnet artifact to the stable channel"
+
+  bio pkg upload --auth "${HABITAT_AUTH_TOKEN}" --url "${HAB_BLDR_URL}" "${hart_file}" -c stable
+
+  # Uploading to the biome builder is currently broken.
+  #This can be uncommented when this bug report is remedied: https://github.com/biome-sh/biome/issues/14
+  #bio pkg upload --auth "${BIOME_AUTH_TOKEN}" --url "${BIOME_BLDR_URL}" "${hart_file}" -c stable
+
+
+  echo "Exporting testnet docker image"
+  sudo bio pkg export container "${hart_file}"
+
+  source results/last_container_export.env
+
+  for tag in ${tags//,/ }; do
+    local_tag="ghcr.io/themeliolabs/themelio-node-testnet:${tag}"
+
+    docker tag "${name}:${tag}" "${local_tag}"
+
+    docker push "${local_tag}"
+  done
+
   for region in $(cat $SCRIPTS_DIRECTORY/packer/aws_regions); do
       echo "Creating packer templates for $region-$NETWORK_TO_BUILD."
 
--- how-to-use/README.md
+++ how-to-use/README.md
@@ -4,9 +4,15 @@ To use our docker image, we have provided a handy docker-compose file.
 
 You will need `docker` and `docker-compose` installed.
 
-Simply download the [compose file](docker-compose.yml), and run:
+
+To run a full node on the MainNet, simply download the [compose file](docker-compose-mainnet.yml), and run:
 ```
-$ docker-compose up
+$ docker-compose up -f docker-compose-mainnet.yml
+```
+
+To run a full node on the TestNet, simply download the [compose file](docker-compose-testnet.yml), and run:
+```
+$ docker-compose up -f docker-compose-testnet.yml
 ```
 
 There are a number of environment variables that are commented out.
