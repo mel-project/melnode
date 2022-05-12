92d75ed6ef9795118756fc9aedfc15ace596e57c
Meade Kincke <thedarkula2049@gmail.com>
2022-05-12 15:20:43 -0700

    Added cargo settings to the remaining plans.

--- biome/themelio-node/plan-debug-testnet.sh
+++ biome/themelio-node/plan-debug-testnet.sh
@@ -26,6 +26,7 @@ pkg_svc_group="$pkg_svc_user"
 do_setup_environment() {
   set_buildtime_env SCCACHE_DIR "/src/sccache"
   set_buildtime_env RUSTC_WRAPPER "$(pkg_path_for core/sccache)/bin/sccache"
+  set_buildtime_env CARGO_HOME "/src/cargo"
 }
 
 do_verify() {
