bec5197784415837a7558f463e97441a2783f6fc
Meade Kincke <thedarkula2049@gmail.com>
2022-06-01 11:41:37 -0700

    Should have the network selection fixed.

--- docker/dev/run.sh
+++ docker/dev/run.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-if [ -n "${ADVERTISE_OFF}" ]; then
-  themelio-node --database /var/lib/themelio-node/main --listen 0.0.0.0:11814
-elif [ -n "${ADVERTISE_MANUAL}" ]; then
-  themelio-node --database /var/lib/themelio-node/main --listen 0.0.0.0:11814 --advertise "${ADVERTISE_MANUAL}":11814
-else
+if [ "${NETWORK}" == "mainnet" ]; then
   PUBLIC_IP_ADDRESS="$(curl -s http://checkip.amazonaws.com)"
   themelio-node --database /var/lib/themelio-node/main --listen 0.0.0.0:11814 --advertise "${PUBLIC_IP_ADDRESS}":11814 &
   sleep 3
   bats --print-output-on-failure /tmp/ci.bats
   exit 0
+elif [ "${NETWORK}" == "testnet" ]; then
+  PUBLIC_IP_ADDRESS="$(curl -s http://checkip.amazonaws.com)"
+  themelio-node --database /var/lib/themelio-node/main --testnet --bootstrap tm-1.themelio.org:11814 --advertise "${PUBLIC_IP_ADDRESS}":11814
+  sleep 3
+  bats --print-output-on-failure /tmp/ci.bats
+  exit 0
+else
+  echo "No network specified with NETWORK. Exiting."
+  exit 1
 fi
